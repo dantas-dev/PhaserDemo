f555b43951610c59d3199ec8505eaeb3236d0e77
Eduardo Dantas <dantasleld@gmail.com>
2018-02-13 23:04:25 -0200

    add SHOW_ALL scale manager and CSS file

--- state0.js
+++ state0.js
@@ -9,6 +9,8 @@ demo.state0.prototype = {
         
     addChangeStateEventListeners();
         
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
     },
     
     update: function(){} 
