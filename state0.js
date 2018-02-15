fc61817ba1c46eb629f1c857ed32cd98191027fe
Eduardo Dantas <dantasleld@gmail.com>
2018-02-14 23:27:07 -0200

    finish state 0

--- state0.js
+++ state0.js
@@ -1,38 +1,65 @@
-var demo = {} , centerX = 1500 / 2 , centerY = 1000 / 2,adam, speed = 4;
+var demo = {} , centerX = 1500 / 2 , centerY = 1000 / 2,adam, speed = 6;
 demo.state0 = function(){};
 
 demo.state0.prototype = {
     preload: function(){
         
-        game.load.image('adam','assets/sprites/adam.png');
+        game.load.spritesheet('adam','assets/spritessheets/adamSheet.png',240,370);
+        game.load.image('tree','assets/backgrounds/treeBG.png');
         
     },
     create: function(){
+        
+    //PHYSICS game
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+        
     game.stage.backgroundColor = '#800080';
     console.log('Stage0');     
         
     addChangeStateEventListeners();
-        
+    
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        
-adam = game.add.sprite(centerX,centerY,'adam');
-adam.anchor.setTo(0.5,0.5);
+    //add BG    
+    var treeBG = game.add.sprite(0,0,'tree');
+    //add sprite player
+    adam = game.add.sprite(centerX,centerY,'adam');
+    adam.anchor.setTo(0.5,0.5);
+    adam.scale.setTo(0.7,0.7);
+    //physics for play
+    game.physics.enable(adam);
+    adam.body.collideWorldBounds = true;
+    //animations adam
+    adam.animations.add('walk',[0,1,2,3,4]);
+    
+    //camera follow player
+    game.camera.follow(adam);
+    game.world.setBounds(0,0,2813,1000);
+    game.camera.deadzone = new Phaser.Rectangle(centerX - 300,0,600,1000);
         
     },
     
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            adam.scale.setTo(0.7,0.7);
             adam.x += speed;
+            adam.animations.play('walk',14,true);
         } else 
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.scale.setTo(-0.7,0.7);
             adam.x -= speed;
+            adam.animations.play('walk',14,true);
+        } else { 
+            adam.animations.stop('walk');
+            adam.frame = 0;
         }
         
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             adam.y -= speed;
+            if(adam.y < 395) {adam.y = 395};
         } else 
         if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             adam.y += speed;
+            if(adam.y > 809) {adam.y = 809};
         }
     } 
 };
