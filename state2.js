a3a8f6d9145f5e3624ac18d9b6baa9b541b0c450
Eduardo Dantas <dantasleld@gmail.com>
2018-02-18 19:08:41 -0300

    add new stages

--- state2.js
+++ state2.js
@@ -1,9 +1,89 @@
+var barrel,velocity = 1000,bullets,nextFire = 0,fireRate = 200,enemy,bullet,enemyGroup;
+
 demo.state2 = function(){};
 demo.state2.prototype = {
-    preload: function(){},
-    create: function(){
-    game.stage.backgroundColor = '#ff8000';
-        addChangeStateEventListeners();
+    preload: function(){
+        //load images
+        game.load.image('base','assets/sprites/cannonBase.png');
+        game.load.image('barrel','assets/sprites/cannonBarrel.png');
+        game.load.image('bullet','assets/sprites/bullet.png');
     },
-    update: function(){} 
+    create: function(){
+    game.stage.backgroundColor = '#d3d3d3';
+        addChangeStateEventListeners();
+        
+        //add base
+        var base = game.add.sprite(centerX,centerY,'base');
+        base.anchor.setTo(0.5);
+        base.scale.setTo(0.4);
+        
+        //add bullets as a group
+        bullets = game.add.group();
+        bullets.enableBody = true;
+        bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        bullets.createMultiple(50, 'bullet'); //(qnt,sprite)
+        bullets.setAll('checkWorldBounds', true);
+        bullets.setAll('outOfBoundsKill', true);
+        bullets.setAll('anchor.y',0.5);
+        bullets.setAll('scale.x',0.85);        
+        bullets.setAll('scale.y',0.85);        
+        //add barrel
+        barrel = game.add.sprite(centerX,centerY,'barrel');        
+        barrel.anchor.setTo(0.3,0.5);
+        barrel.scale.setTo(0.5);
+        
+        //add enemy
+        enemy = game.add.sprite(100,200,'adam');
+        game.physics.enable(enemy);
+        
+        //add enemy in group
+        enemyGroup = game.add.group();
+        enemyGroup.enableBody = true;
+        enemyGroup.physicsBodyType = Phaser.Physics.ARCADE;
+        for(var i = 0; i < 3; i++){
+            enemyGroup.create(1300,350 * i + 100,'adam');
+        }
+        enemyGroup.setAll('anchor.x',0.5);
+        enemyGroup.setAll('anchor.y',0.5);
+        enemyGroup.setAll('scale.y',0.4);
+        enemyGroup.setAll('scale.x',0.4);
+        
+
+        
+    },
+    update: function(){
+        //set rotation to barrel
+        barrel.rotation = game.physics.arcade.angleToPointer(barrel);
+        //call fire function if click mouse
+        if(game.input.activePointer.isDown){
+            this.fire();
+           }
+        
+        //if bullet overlap enemy
+        game.physics.arcade.overlap(bullets,enemy,this.hitEnemy);
+        game.physics.arcade.overlap(bullets,enemyGroup,this.hitGroup);
+    } ,
+    
+    fire: function(){
+        if (game.time.now > nextFire) {
+            nextFire = game.time.now + fireRate;
+            console.log('firing');
+            bullet = bullets.getFirstDead();
+            bullet.reset(barrel.x, barrel.y);
+
+            game.physics.arcade.moveToPointer(bullet, velocity);
+            bullet.rotation = game.physics.arcade.angleToPointer(bullet);
+
+        }
+    },
+    
+    hitEnemy: function(){
+        console.log('hit');
+        enemy.kill();
+        bullet.kill();
+    },
+    hitGroup: function(b,e){
+        b.kill(); //bullet
+        e.kill(); //enemyGroup
+    }
 };
--- state4.js
+++ state4.js
@@ -1,9 +1,29 @@
+var i;
+
 demo.state4 = function(){};
 demo.state4.prototype = {
-    preload: function(){},
-    create: function(){
-    game.stage.backgroundColor = '#ffff00';
-        addChangeStateEventListeners();
+  preload: function(){},
+  create: function(){
+    game.stage.backgroundColor = '#cc66ff';
+    addChangeStateEventListeners();
+      
+    /* myA = game.add.sprite(25,50,'adam');
+    game.add.tween(myA).to({x: 800},3000,'Linear',true);
+    */
+    a1 = game.add.sprite(50, 100, 'adam');
+    a2 = game.add.sprite(350, 100, 'adam');
+    a3 = game.add.sprite(650, 100, 'adam');
+    a4 = game.add.sprite(950, 100, 'adam');
+    a5 = game.add.sprite(1250, 100, 'adam');
+      
+    game.add.tween(a1).to({y: 400},2000,'Cubic.easeInOut',true);
+    i = game.add.tween(a2).to({x: 100,y: 0},1000,'Elastic.easeOut');
+      
+    game.add.tween(a3).from({y: 1000},1500,'Circ.easeOut',true);
+    
+   game.add.tween(a4.anchor).to({x: 1},1000,'Linear',true,1000,2,true); 
+      
+    game.add.tween(a5).to({alpha:0},1000,'Bounce',true,1000,false,true).loop(true);
     },
     update: function(){} 
 };
